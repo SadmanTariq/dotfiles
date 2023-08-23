d25fef64cf685bfa54d1e7745000caee1d4c3961
Sadman Tariq <sadmantariq@gmail.com>
2023-08-23 10:52:21 +1000

    Change picom config and add nvchad.

---
+++ .config/nvim/lua/custom/chadrc.lua
@@ -0,0 +1,27 @@
+---@type ChadrcConfig
+local M = {}
+
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
+M.ui = {
+  theme = "gruvbox",
+  theme_toggle = { "gruvbox", "one_light" },
+
+  hl_override = highlights.override,
+  hl_add = highlights.add,
+
+  transparency = true,
+
+
+  statusline = {
+    theme = "minimal"
+  }
+}
+
+M.plugins = "custom.plugins"
+
+-- check core.mappings for table structure
+M.mappings = require "custom.mappings"
+
+return M
