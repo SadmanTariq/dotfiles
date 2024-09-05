fae0d8b47c9cb4a48861efba2ba2ba57c77fe07d
Sadman Tariq <sadmantariq@gmail.com>
2024-09-06 08:13:34 +1000

    Only load image.nvim on kitty term.

--- .config/nvim/lua/plugins/image.lua
+++ .config/nvim/lua/plugins/image.lua
@@ -1,7 +1,11 @@
-return {
-    "3rd/image.nvim",
-    dependencies = { "luarocks.nvim" },
-    config = function()
-        require("image").setup()
-    end
-}
+if vim.env.TERMINAL == 'kitty' then
+    return {
+        "3rd/image.nvim",
+        dependencies = { "luarocks.nvim" },
+        config = function()
+            require("image").setup()
+        end
+    }
+else
+    return {}
+end
